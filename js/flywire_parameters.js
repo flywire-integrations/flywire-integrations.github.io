f7abf9157a7e24a9700cb488ee42cd2ee2c817ec
gtfoo-flywire <guoting.foo@flywire.com>
2020-09-16 17:36:23 +0800

    more refactoring

--- js/flywire_parameters.js
+++ js/flywire_parameters.js
@@ -2,10 +2,10 @@ portal_code = "FCL";
 payment_destination = "flywire-college";
 env = "demo";
 locale = "en";
-callback_url = "https =//hook.integromat.com/3nofk17o6bhbbocph5aux1uhsqbx6cf9";
-return_url = "https =//flywire-integrations.github.io/invoices_paid.html";
+callback_url = "https://hook.integromat.com/3nofk17o6bhbbocph5aux1uhsqbx6cf9";
+return_url = "https://flywire-integrations.github.io/invoices_paid.html";
 
-amount = "250000";
+amount = "2500";
 
 sender_country = 'CN';
 sender_first_name = "John";
@@ -22,3 +22,8 @@ student_email = "johnsmith@flywire.com";
 student_id = "A123456700";
 student_first_name = "John";
 student_last_name = "Smith";
+invoice_no = "FC-10005001";
+payment_for = "Winter 2020 tuition";
+
+//Invoice Paid page
+flywire_id = "FCL123456789";
